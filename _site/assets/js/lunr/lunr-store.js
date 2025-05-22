70795efb0fd77957f5a330a9182a629936afe4b6
Nikolai Korolev <korolev.n@hotmail.com>
2025-05-22 14:15:04 +0300

    Init

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Мой первый пост",
+        "excerpt":"Привет!  ","categories": ["блог"],
+        "tags": ["go","obsidian"],
+        "url": "/%D0%B1%D0%BB%D0%BE%D0%B3/2025/05/22/my-first-post.html",
+        "teaser": null
+      }]
