88778fa65d3636a9d7cbad407a8dd0b7d2a5d603
Nikolai Korolev <korolev.n@hotmail.com>
2025-05-22 14:42:26 +0300

    fix

--- _site/README.md
+++
@@ -1,1 +0,0 @@
-# korolev-n.github.io
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Мой первый пост",
         "excerpt":"Привет!  ","categories": ["блог"],
         "tags": ["go","obsidian"],
-        "url": "/%D0%B1%D0%BB%D0%BE%D0%B3/2025/05/22/my-first-post.html",
+        "url": "/%D0%B1%D0%BB%D0%BE%D0%B3/2025/05/22/my-first-post/",
         "teaser": null
       }]
